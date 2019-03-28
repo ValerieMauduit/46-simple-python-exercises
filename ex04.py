7ed01216823e61657d829c5db94108b27dc27425
rrsw <mboivin@student.42.fr>
2019-03-28 14:54:07 +0100

    Added ex04

---
+++ ex04.py
@@ -0,0 +1,19 @@
+"""Write a function that takes a character (i.e. a string of length 1)
+and returns True if it is a vowel, False otherwise."""
+
+def is_vowel(char):
+    """
+    This function returns True if the character is a vowel
+    y can be added in the vowel list if necessary
+
+    Parameters
+    ----------
+    a character (string of length 1)
+
+    Returns
+    ----------
+    True if the character is a vowel
+    False otherwise
+    """
+    vowels = ["a", "e", "i", "o", "u"]
+    return char in vowels
