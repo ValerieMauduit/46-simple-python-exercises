a4dcdb952ca260b9e6cb8b1c4706059cb43e78a0
rrsw <mboivin@student.42.fr>
2019-03-28 23:15:16 +0100

    Added ex07

---
+++ ex07.py
@@ -0,0 +1,16 @@
+"""Define a function reverse() that computes the reversal of a string.
+For example, reverse("I am testing") should return the string "gnitset ma I"."""
+
+def reverse(txt):
+    """
+    This function computes the reversal of a string
+
+    Parameters
+    ----------
+    txt (string)
+
+    Returns
+    ----------
+    The reversed string
+    """
+    return txt[::-1]
