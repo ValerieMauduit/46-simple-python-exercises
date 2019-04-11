24ba10c0fd30fa181e026513c836621427df2c16
rrsw <mboivin@student.42.fr>
2019-04-11 17:27:34 +0200

    Added ex20

---
+++ ex20.py
@@ -0,0 +1,32 @@
+'''Represent a small bilingual lexicon as a Python dictionary in the following
+fashion {"merry":"god", "christmas":"jul", "and":"och", "happy":gott",
+"new":"nytt", "year":"år"} and use it to translate your Christmas cards from
+English into Swedish. That is, write a function translate() that takes a list of
+English words and returns a list of Swedish words.'''
+
+def translate(lst):
+    '''
+    This function translates English words into Swedish words
+
+    Parameters
+    ----------
+    lst (list of strings)
+
+    Returns
+    ----------
+    Translated list
+    '''
+    xmas_dict = {
+        'merry': 'god',
+        'christmas': 'jul',
+        'and': 'och',
+        'happy': 'gott',
+        'new': 'nytt',
+        'year': 'år'
+        }
+    return [xmas_dict[w.lower()] for w in lst]
+
+'''Uncomment the following line to test the function and try to change the
+list of strings passed to the translate() function.'''
+
+# print(translate(['Merry', 'christmas', 'and', 'happy', 'new', 'year']))
