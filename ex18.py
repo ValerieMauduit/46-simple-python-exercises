d6e31c585abb6c222164dec267a162dea95a6116
rrsw <mboivin@student.42.fr>
2019-04-11 17:06:21 +0200

    Added ex18

---
+++ ex18.py
@@ -0,0 +1,30 @@
+'''A pangram is a sentence that contains all the letters of the English alphabet
+at least once, for example: "The quick brown fox jumps over the lazy dog".
+Your task here is to write a function to check a sentence to see if it is
+a pangram or not.'''
+
+
+def is_pangram(str):
+  '''
+  This function recognizes pangrams
+  (i.e. a sentence that contains all the letters of the English alphabet
+  at least once)
+
+  Parameters
+  ----------
+  str (string)
+
+  Returns
+  ----------
+  True if the sentence is a pangram
+  False otherwise
+  '''
+  phrase = [i.lower() for i in str if i.isalpha()]
+  clean = set(phrase)
+  return len(clean) == 26
+
+'''Uncomment the following lines to test the function and try to change the
+string passed to the is_pangram() function.'''
+
+# print(is_pangram("The quick brown fox jumps over the lazy dog"))
+# print(is_pangram("You are searching for a random sentence"))
